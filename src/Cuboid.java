07f5ab64f194bb2c42c5960e59ed3bd0395c6a93
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-20 14:32:03 +0300

    Rectangle and Cuboid classes created

---
+++ src/Cuboid.java
@@ -0,0 +1,16 @@
+public class Cuboid extends Rectangle{
+    double height;
+
+    public Cuboid(double width, double length, double height){
+        super(width, length);
+        this.height = Helper.checkValue(height);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+        return getArea() * getHeight();
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,21 @@
+public class Rectangle {
+    double width;
+    double length;
+
+    public Rectangle(double width, double length){
+        this.width = Helper.checkValue(width);
+        this.length = Helper.checkValue(length);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea(){
+        return width * length;
+    }
+}
