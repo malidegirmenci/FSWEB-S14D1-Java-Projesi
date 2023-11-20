55e940146b4fa5ca8b46b94468015735d16ffb31
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-20 14:31:52 +0300

    Rectangle and Cuboid classes tested

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        /*             TESTS PASSED
+        /*  CIRCLE AND CYLINDER CLASSES TESTS PASSED
+
             Circle circle = new Circle(3.75);
 
             System.out.println("circle.radius= " + circle.getRadius());
@@ -17,6 +18,29 @@ public class Main {
 
             System.out.println("cylinder.volume= " + cylinder.getVolume());
        */
+
+        /*  RECTANGLE AND CUBOID CLASSES TESTS PASSED
+        Rectangle rectangle = new Rectangle(5, 10);
+
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+
+        System.out.println("rectangle.length= " + rectangle.getLength());
+
+        System.out.println("rectangle.area= " + rectangle.getArea());
+
+        Cuboid cuboid = new Cuboid(5,10,5);
+
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+
+        System.out.println("cuboid.length= " + cuboid.getLength());
+
+        System.out.println("cuboid.area= " + cuboid.getArea());
+
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
+
+        */
 
     }
 }
